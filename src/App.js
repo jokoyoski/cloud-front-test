9bd06aec2cb8605aa0f505e2ae0b22ebf05c368d
joko <joko>
2024-01-21 15:40:00 +0000

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Emi lege
+         Emi lege120
         </p>
         <a
           className="App-link"
